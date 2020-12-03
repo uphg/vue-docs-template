355f0baccd4d517830859fbbb8ef5f371b6a14fd
ChenNing02 <2633565885@qq.com>
2020-12-03 17:03:09 +0800

    deploy

---
+++ service-worker.js
@@ -0,0 +1,641 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [
+  {
+    "url": "404.html",
+    "revision": "9481a2a9b615f96490d69071776fd6f7"
+  },
+  {
+    "url": "assets/css/0.styles.bb6a8b77.css",
+    "revision": "dd347d2e01e28fb7a287625c7e1a9117"
+  },
+  {
+    "url": "assets/img/search.83621669.svg",
+    "revision": "83621669651b9a3d4bf64d1a670ad856"
+  },
+  {
+    "url": "assets/js/10.c58dbb3f.js",
+    "revision": "f5458cf2b8101e4350d13dc8259dc9cf"
+  },
+  {
+    "url": "assets/js/11.7b592c3b.js",
+    "revision": "a4eafdba4bb804371331e07f1761973b"
+  },
+  {
+    "url": "assets/js/12.68f9529c.js",
+    "revision": "a84bf9692e6b62e74f6bc8e96457fd42"
+  },
+  {
+    "url": "assets/js/13.019cca47.js",
+    "revision": "1306fef9a9631811e3f8d314d9d52555"
+  },
+  {
+    "url": "assets/js/14.01e71cdf.js",
+    "revision": "70b84c8ddcf379ac13dcc2199e157b60"
+  },
+  {
+    "url": "assets/js/15.823aca0b.js",
+    "revision": "ad790e7b7c26dd906153cf16feb12417"
+  },
+  {
+    "url": "assets/js/16.5b9e3457.js",
+    "revision": "74a40aa02f26f36737dc7519fffa599d"
+  },
+  {
+    "url": "assets/js/17.c51e6e5c.js",
+    "revision": "f5bfceba23c9669dccaf07a6d4c30d4f"
+  },
+  {
+    "url": "assets/js/2.be9e6e2c.js",
+    "revision": "1f4453deb9988dbf414bc272b0688e18"
+  },
+  {
+    "url": "assets/js/3.a724c90b.js",
+    "revision": "d988c93fa06e168b996a1ec45471fb48"
+  },
+  {
+    "url": "assets/js/4.624b6d51.js",
+    "revision": "ffc71dc498eb8ed73b2381c0c6cedd53"
+  },
+  {
+    "url": "assets/js/5.9bb7cd1d.js",
+    "revision": "f9f8b6bb6c5ed0a534e96d2277152dc7"
+  },
+  {
+    "url": "assets/js/6.5ce77b0d.js",
+    "revision": "de04e82db5e5264d03bba8690ace720b"
+  },
+  {
+    "url": "assets/js/7.d9fcdc89.js",
+    "revision": "b6c85e054c27524baefefcb830a2fbdf"
+  },
+  {
+    "url": "assets/js/8.c4e7202d.js",
+    "revision": "ba7bed4bbbbd7f00f83b0d341522c15f"
+  },
+  {
+    "url": "assets/js/9.74e3dbd9.js",
+    "revision": "9c543c0f80328c5565ed41cb9bb21baa"
+  },
+  {
+    "url": "assets/js/app.ceb3dc2c.js",
+    "revision": "f81694f67c30fc76dc025e8ade8802e1"
+  },
+  {
+    "url": "bar/index.html",
+    "revision": "7d0934e35830e2a8e3154af710dc02e1"
+  },
+  {
+    "url": "bar/markdown.html",
+    "revision": "e9eeed9b6877f043c8a2cfee50248164"
+  },
+  {
+    "url": "bar/one.html",
+    "revision": "e8305bbf6c7323c2013429705232e915"
+  },
+  {
+    "url": "bar/two.html",
+    "revision": "1817d92554a761d0451768138e753703"
+  },
+  {
+    "url": "foo/api/index.html",
+    "revision": "adc080ee1be931074d5d17e4a56b5144"
+  },
+  {
+    "url": "foo/api/one.html",
+    "revision": "3d2939a9b5eff91ba0a58d2784578968"
+  },
+  {
+    "url": "foo/api/two.html",
+    "revision": "343a004093deb36f1d0972bade9c980c"
+  },
+  {
+    "url": "foo/more/index.html",
+    "revision": "c100e7638af4dd85beb457b00e3d714b"
+  },
+  {
+    "url": "foo/more/one.html",
+    "revision": "647b8410aab12c09edc7d26ff55ccb36"
+  },
+  {
+    "url": "foo/more/two.html",
+    "revision": "6577b08d6fb36e0e358badb46fedd59c"
+  },
+  {
+    "url": "images/AccessibilityChromeDeveloperTools.png",
+    "revision": "25c2a61b52ea8753aa4693a16abaa43f"
+  },
+  {
+    "url": "images/AccessibleARIAdescribedby.png",
+    "revision": "d2b26eb9ae0006509801691c289a86d3"
+  },
+  {
+    "url": "images/AccessibleARIAlabelDevTools.png",
+    "revision": "05cb34b380cef9627d5c9a3c0ba64dca"
+  },
+  {
+    "url": "images/AccessibleARIAlabelledbyDevTools.png",
+    "revision": "1554e00985256ca1042caffbf59709cc"
+  },
+  {
+    "url": "images/AccessibleLabelChromeDevTools.png",
+    "revision": "5b9d491c368093887624f4dfacdb6431"
+  },
+  {
+    "url": "images/breakpoint_hit.png",
+    "revision": "114924925a4ec0f23236012bc3dc8422"
+  },
+  {
+    "url": "images/breakpoint_set.png",
+    "revision": "6439856732303cfeb3806d52dd681191"
+  },
+  {
+    "url": "images/components_provide.png",
+    "revision": "f7110a1bae2d0744997012ca656d8fa1"
+  },
+  {
+    "url": "images/components.png",
+    "revision": "b5c08269dfc26ae6d7db3801e9efd296"
+  },
+  {
+    "url": "images/config_add.png",
+    "revision": "353cd8b2a1bdf9fc4c74a80c5f38090a"
+  },
+  {
+    "url": "images/css-vs-js-ease.svg",
+    "revision": "d326f3556b9001865ed2fd91faebf620"
+  },
+  {
+    "url": "images/devtools-timetravel.gif",
+    "revision": "fca84f3fb8a8d10274eb2fc7ed9b65f9"
+  },
+  {
+    "url": "images/dom-tree.png",
+    "revision": "f70b86bfbbfe1962dc5d6125105f1122"
+  },
+  {
+    "url": "images/icons/android-icon-144x144.png",
+    "revision": "e67b8d54852c2fbf40be2a8eb0590f5b"
+  },
+  {
+    "url": "images/icons/android-icon-192x192.png",
+    "revision": "5d10eaab941eb596ee59ffc53652d035"
+  },
+  {
+    "url": "images/icons/android-icon-36x36.png",
+    "revision": "bb757d234def1a6b53d793dbf4879578"
+  },
+  {
+    "url": "images/icons/android-icon-48x48.png",
+    "revision": "0d33c4fc51e2bb020bf8dd7cd05db875"
+  },
+  {
+    "url": "images/icons/android-icon-72x72.png",
+    "revision": "702c4fafca31d670f9bd8b2d185ced39"
+  },
+  {
+    "url": "images/icons/android-icon-96x96.png",
+    "revision": "0ebff702851985ea6ba891cf6e6e7ddd"
+  },
+  {
+    "url": "images/icons/apple-icon-114x114.png",
+    "revision": "f4fd30f3a26b932843b8c5cef9f2186e"
+  },
+  {
+    "url": "images/icons/apple-icon-120x120.png",
+    "revision": "b6a574d63d52ef9c89189b67bcac5cbd"
+  },
+  {
+    "url": "images/icons/apple-icon-144x144.png",
+    "revision": "e67b8d54852c2fbf40be2a8eb0590f5b"
+  },
+  {
+    "url": "images/icons/apple-icon-152x152.png",
+    "revision": "f53787bf41febf2b044931a305ccaf2a"
+  },
+  {
+    "url": "images/icons/apple-icon-180x180.png",
+    "revision": "9f6b1e3b92b2c5bd5b7d79501bb6e612"
+  },
+  {
+    "url": "images/icons/apple-icon-57x57.png",
+    "revision": "83f622ba0994866abc56ace068b6551c"
+  },
+  {
+    "url": "images/icons/apple-icon-60x60.png",
+    "revision": "643f761bc39f86c70f17cd1fed3b8e08"
+  },
+  {
+    "url": "images/icons/apple-icon-72x72.png",
+    "revision": "702c4fafca31d670f9bd8b2d185ced39"
+  },
+  {
+    "url": "images/icons/apple-icon-76x76.png",
+    "revision": "94d9af047b86d99657b5efb88a0d1c7b"
+  },
+  {
+    "url": "images/icons/apple-icon-precomposed.png",
+    "revision": "707758f591323153a4f1cb3a8d9641fa"
+  },
+  {
+    "url": "images/icons/apple-icon.png",
+    "revision": "707758f591323153a4f1cb3a8d9641fa"
+  },
+  {
+    "url": "images/icons/bacancy_technology.png",
+    "revision": "5810bb8253b1e35ba437373ff83f82d3"
+  },
+  {
+    "url": "images/icons/bulb.svg",
+    "revision": "e3fbc07fc8a4fd64bf3585de3c5868c4"
+  },
+  {
+    "url": "images/icons/danger.svg",
+    "revision": "7224901b2f84983efa258490e5a1c03a"
+  },
+  {
+    "url": "images/icons/favicon-16x16.png",
+    "revision": "a5a9da66870189b0539223c38c8a7749"
+  },
+  {
+    "url": "images/icons/favicon-32x32.png",
+    "revision": "3d60db0d77303b2414ddd50cf2472bf7"
+  },
+  {
+    "url": "images/icons/favicon-96x96.png",
+    "revision": "0ebff702851985ea6ba891cf6e6e7ddd"
+  },
+  {
+    "url": "images/icons/info.svg",
+    "revision": "39d1a12b97555d796d4dcda9bf403429"
+  },
+  {
+    "url": "images/icons/ms-icon-144x144.png",
+    "revision": "e67b8d54852c2fbf40be2a8eb0590f5b"
+  },
+  {
+    "url": "images/icons/ms-icon-150x150.png",
+    "revision": "e8cdf492981122a2548bc247c7b4067d"
+  },
+  {
+    "url": "images/icons/ms-icon-310x310.png",
+    "revision": "1721f8303ec2349002b5980a01f27cef"
+  },
+  {
+    "url": "images/icons/ms-icon-70x70.png",
+    "revision": "a110cf0132b00b23a8605ca72a8874ba"
+  },
+  {
+    "url": "images/icons/stop.svg",
+    "revision": "7d577a225ddf7f95f96032b3eefb48b6"
+  },
+  {
+    "url": "images/lifecycle.png",
+    "revision": "55ca3bcd54e2ee9bd7e3575eb02a1e13"
+  },
+  {
+    "url": "images/partners/monterail.png",
+    "revision": "db165491374f80cc4f3190a0ebd918ad"
+  },
+  {
+    "url": "images/partners/vehikl.png",
+    "revision": "65f4ae56972001f689053fba43129433"
+  },
+  {
+    "url": "images/paypal.png",
+    "revision": "067bd556ce9e4c76538a8057adb6d596"
+  },
+  {
+    "url": "images/scoped-slot.png",
+    "revision": "c6ef14ba02eac288245c5c5009d966cc"
+  },
+  {
+    "url": "images/sfc-with-preprocessors.png",
+    "revision": "2e57ecfafeac2237d5a003ad9a0ef7bc"
+  },
+  {
+    "url": "images/sfc.png",
+    "revision": "e333ce3bf8119bef381ac7c7b2bbd4ba"
+  },
+  {
+    "url": "images/slot.png",
+    "revision": "00cf6bd787014eb22b2821d72b80212a"
+  },
+  {
+    "url": "images/sponsors/autocode.svg",
+    "revision": "e1c1c17d96d289b20b2d91819a4c9e4d"
+  },
+  {
+    "url": "images/sponsors/bacancy_technology.png",
+    "revision": "9a0590eb4ce29289b454240415611162"
+  },
+  {
+    "url": "images/sponsors/bestvpn_co.png",
+    "revision": "afbe252b6b59bc3cdac2e7dec69eac39"
+  },
+  {
+    "url": "images/sponsors/bit.png",
+    "revision": "9638a3f44bf471876effb80ea0659f73"
+  },
+  {
+    "url": "images/sponsors/chaitin.png",
+    "revision": "549e43997790dc624c477424acbfe228"
+  },
+  {
+    "url": "images/sponsors/cloudstudio.png",
+    "revision": "fc480cf4c2b06591f58e7e91666226af"
+  },
+  {
+    "url": "images/sponsors/dcloud.gif",
+    "revision": "ade7c64e66506b6cff10292efea16ee8"
+  },
+  {
+    "url": "images/sponsors/devexpress.png",
+    "revision": "a6d9c786a373088c8d238ca643293c17"
+  },
+  {
+    "url": "images/sponsors/devsquad.png",
+    "revision": "e639ea4fd0d7053fc0928d4ff9fefb2a"
+  },
+  {
+    "url": "images/sponsors/fastcoding_inc.png",
+    "revision": "08a0a7652db79fa3395c0ef28d49f0cd"
+  },
+  {
+    "url": "images/sponsors/fastcoding_inc.svg",
+    "revision": "9d33d7905c4fb224aba61de096505794"
+  },
+  {
+    "url": "images/sponsors/firestick_tricks.png",
+    "revision": "1ee05223a5b12fe910cb8428d57223d8"
+  },
+  {
+    "url": "images/sponsors/flatlogic_templates.svg",
+    "revision": "4442dca91b270a32353ee5aca44ebc33"
+  },
+  {
+    "url": "images/sponsors/foo.png",
+    "revision": "1c9cde53bb9c98a316edc93d57684e4d"
+  },
+  {
+    "url": "images/sponsors/frontend_love.png",
+    "revision": "b514babc53a0f3ddc854b0b14a54a489"
+  },
+  {
+    "url": "images/sponsors/frontendlove.png",
+    "revision": "b514babc53a0f3ddc854b0b14a54a489"
+  },
+  {
+    "url": "images/sponsors/gridsome.png",
+    "revision": "e82a2f872ec319bbb5d0a804288cd9b7"
+  },
+  {
+    "url": "images/sponsors/happy_programmer_llc.png",
+    "revision": "3f3303d42a57ff9edf36373f59d376af"
+  },
+  {
+    "url": "images/sponsors/html_burger.png",
+    "revision": "c7ce1344d001e7a236a89694ed59d988"
+  },
+  {
+    "url": "images/sponsors/icons_8.png",
+    "revision": "ffcdd01817ecdb32b92bd2f1e4d75e84"
+  },
+  {
+    "url": "images/sponsors/icons.png",
+    "revision": "ad6ee8c400066e15712cdef4342023da"
+  },
+  {
+    "url": "images/sponsors/inkoop.png",
+    "revision": "1cff77d2c927657d3aceeba2c12db892"
+  },
+  {
+    "url": "images/sponsors/intygrate.png",
+    "revision": "fdd390b44a4aeed763f53f4e8f6529e4"
+  },
+  {
+    "url": "images/sponsors/isolutions_uk_limited.png",
+    "revision": "0f76512940c38b72fcf48337b4d64692"
+  },
+  {
+    "url": "images/sponsors/laravel.png",
+    "revision": "26ece85fc10e3d137564a414a0168b7b"
+  },
+  {
+    "url": "images/sponsors/modus.png",
+    "revision": "6498c04fee5b8542449b350e77180379"
+  },
+  {
+    "url": "images/sponsors/Monterail.png",
+    "revision": "bf1ec94a0ca48f0e6be0c97fa60a42c0"
+  },
+  {
+    "url": "images/sponsors/moovweb.png",
+    "revision": "8183954731fdeb0f136fac1485198184"
+  },
+  {
+    "url": "images/sponsors/neds.png",
+    "revision": "1f1a2a46c2575019ae07a90205f60b65"
+  },
+  {
+    "url": "images/sponsors/onsen_ui.png",
+    "revision": "e41569bcb10fbca3f361d818b29ed7fd"
+  },
+  {
+    "url": "images/sponsors/passionate_people.png",
+    "revision": "03e59e28347e1dcd165e4e1525afb545"
+  },
+  {
+    "url": "images/sponsors/primevue copy.png",
+    "revision": "60f2e8fb0dce3e9045fc3a2a8039fa82"
+  },
+  {
+    "url": "images/sponsors/primevue.png",
+    "revision": "60f2e8fb0dce3e9045fc3a2a8039fa82"
+  },
+  {
+    "url": "images/sponsors/programmers_io.png",
+    "revision": "02cb415eb9a8e9ce6579c7aff03759dd"
+  },
+  {
+    "url": "images/sponsors/pullrequest.svg",
+    "revision": "50847513b306736d33234d50b11c5e1d"
+  },
+  {
+    "url": "images/sponsors/retool.png",
+    "revision": "aaad6a749deb625da5771750dcb51920"
+  },
+  {
+    "url": "images/sponsors/roadster.png",
+    "revision": "080fb711e736d686f182358a582d7c6b"
+  },
+  {
+    "url": "images/sponsors/shopware_ag.png",
+    "revision": "e2ded483c0660bd629938e37f388d9fb"
+  },
+  {
+    "url": "images/sponsors/storekit.png",
+    "revision": "cacf47116e5efe9fc2dcd60ebc197707"
+  },
+  {
+    "url": "images/sponsors/storyblok.png",
+    "revision": "64ec1772109b769e91138b58526484ad"
+  },
+  {
+    "url": "images/sponsors/tidelift.png",
+    "revision": "831935bd53d7d2d4eea9427c5f874816"
+  },
+  {
+    "url": "images/sponsors/tighten_co.png",
+    "revision": "003364e7044150e2979cbfe03d640cec"
+  },
+  {
+    "url": "images/sponsors/tooltwist.png",
+    "revision": "b81bfd5ae608e965d03aaa5a4164373e"
+  },
+  {
+    "url": "images/sponsors/unicorn.png",
+    "revision": "e0c072bd78f366471a393b9c366c9b74"
+  },
+  {
+    "url": "images/sponsors/usave.png",
+    "revision": "5cffd5053b1d75ae49c9b6eb176e0ccf"
+  },
+  {
+    "url": "images/sponsors/valuecoders.png",
+    "revision": "818ca42a745e018ace0c55c36a7ae3dd"
+  },
+  {
+    "url": "images/sponsors/vehikl.png",
+    "revision": "3bd1b88aa9d242d308e838f2342d7660"
+  },
+  {
+    "url": "images/sponsors/vpnranks.png",
+    "revision": "35d7392e773d487e13358d8b5f7fb646"
+  },
+  {
+    "url": "images/sponsors/vuejobs.png",
+    "revision": "77ed618e17571d1a2d77ad7bc53e8fc4"
+  },
+  {
+    "url": "images/sponsors/vuemastery.png",
+    "revision": "6f09ce143467fba22039bde3f2070c19"
+  },
+  {
+    "url": "images/sponsors/vueschool.png",
+    "revision": "3d92b4f1a8fcbe3be0d0e89950a1a705"
+  },
+  {
+    "url": "images/sponsors/vuetify.png",
+    "revision": "c7cfff77abb10162cb0b7c2ed3b6ac51"
+  },
+  {
+    "url": "images/sponsors/watchcartoononline.png",
+    "revision": "f7cf1082b14003908496f02f9eb2ae00"
+  },
+  {
+    "url": "images/sponsors/webdock.png",
+    "revision": "6b8d3d271ba4d05daf83ad75d21221d1"
+  },
+  {
+    "url": "images/sponsors/webucator.png",
+    "revision": "3c87885f4c36bc1b07f8c2b547e84b6f"
+  },
+  {
+    "url": "images/sponsors/wilderminds.png",
+    "revision": "cd98b69653b51369da2e765097f13d6f"
+  },
+  {
+    "url": "images/sponsors/writers_per_hour.jpg",
+    "revision": "2033e6d7e88969e97e78e38d8d030eb9"
+  },
+  {
+    "url": "images/sponsors/x_team.png",
+    "revision": "a6cfaebb0c0dc17d348bc9c6fd5758ef"
+  },
+  {
+    "url": "images/sponsors/y8.png",
+    "revision": "3cdd8826d3419751f40a8aa7f90cd539"
+  },
+  {
+    "url": "images/sponsors/yakaz.png",
+    "revision": "f1918919114e35d6091e67370450e8bd"
+  },
+  {
+    "url": "images/state.png",
+    "revision": "6a05b01942c7d2cff4ea0033ded59ebb"
+  },
+  {
+    "url": "images/transition.png",
+    "revision": "5990c1dff7dc7a8fb3b34b4462bd0105"
+  },
+  {
+    "url": "images/transitions.svg",
+    "revision": "f0002fdb27ed433579d15f67c3b06153"
+  },
+  {
+    "url": "images/v-bind-instead-of-sync.png",
+    "revision": "cb59705b61fd5a75b1903f6a0b497cb1"
+  },
+  {
+    "url": "images/vuemastery/background.png",
+    "revision": "715d4f2f2e8074c3530f7ab501df40a0"
+  },
+  {
+    "url": "images/vuemastery/black-hole.png",
+    "revision": "61e9fcaa8187ffcbbfc80b30f78271b8"
+  },
+  {
+    "url": "images/vuemastery/logo-vuemastery.svg",
+    "revision": "17c3a6d15462844a32874697b163f5d8"
+  },
+  {
+    "url": "images/vuemastery/planets.png",
+    "revision": "55d65a192a748f2907a6e787ef2d3472"
+  },
+  {
+    "url": "index.html",
+    "revision": "273366af00cd8e5648aa0b318e052373"
+  },
+  {
+    "url": "logo.png",
+    "revision": "cf23526f451784ff137f161b8fe18d5a"
+  }
+].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+addEventListener('message', event => {
+  const replyPort = event.ports[0]
+  const message = event.data
+  if (replyPort && message && message.type === 'skip-waiting') {
+    event.waitUntil(
+      self.skipWaiting().then(
+        () => replyPort.postMessage({ error: null }),
+        error => replyPort.postMessage({ error })
+      )
+    )
+  }
+})
